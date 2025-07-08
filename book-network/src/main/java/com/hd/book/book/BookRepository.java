087d043cf44587effbc71566f1db0b5e63362362
handi <3283606583@qq.com>
2025-07-08 11:25:34 +0800

    使用 BookSpecification

--- book-network/src/main/java/com/hd/book/book/BookRepository.java
+++ book-network/src/main/java/com/hd/book/book/BookRepository.java
@@ -3,10 +3,11 @@ package com.hd.book.book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface BookRepository extends JpaRepository<Book, Integer> {
+public interface BookRepository extends JpaRepository<Book, Integer>, JpaSpecificationExecutor<Book> {
 
     @Query("""
            select book
